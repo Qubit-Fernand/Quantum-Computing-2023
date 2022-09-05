6682964bec3ffab57bfc3a3df57dae17fea98ae2
AntiEntropy-bit <AntiEntropy@pku.edu.cn>
2022-09-05 14:18:13 +0800

    recover tableofcontent

--- components/NotionPage.tsx
+++ components/NotionPage.tsx
@@ -196,7 +196,7 @@ export const NotionPage: React.FC<types.PageProps> = ({
     block?.type === 'page' && block?.parent_table === 'collection'
 
   const showTableOfContents = !!isBlogPost
-  const minTableOfContentsItems = 1
+  const minTableOfContentsItems = 3
 
   const pageAside = React.useMemo(
     () => (
