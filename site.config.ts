800201933d9f241258458c219cf1747abeb441b3
AntiEntropy-bit <AntiEntropy@pku.edu.cn>
2022-09-13 21:59:13 +0800

    try

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   // twitter: 'transitive_bs',
-  // github: 'transitive-bullshit',
+  github: 'Qubit-Fernand',
   // linkedin: 'fisch2',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
