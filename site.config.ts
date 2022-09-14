f7bc468c44c22dcdf94be698ac726592a03e2c20
AntiEntropy-bit <AntiEntropy@pku.edu.cn>
2022-09-14 20:12:51 +0800

    customize

--- site.config.ts
+++ site.config.ts
@@ -18,9 +18,9 @@ export default siteConfig({
 
   // social usernames (optional)
 
-  // twitter: 'transitive_bs',
+  twitter: 'transitive_bs',
   github: 'Qubit-Fernand',
-  // linkedin: 'fisch2',
+  linkedin: 'fisch2',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
@@ -50,16 +50,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+    },
+    {
+      title: 'Contact',
+      pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+    }
+  ]
 })
