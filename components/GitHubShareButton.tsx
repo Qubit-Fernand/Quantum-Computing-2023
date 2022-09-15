51d65cc0e17de116b9fcc0da37142d4533e4199f
AntiEntropy-bit <AntiEntropy@pku.edu.cn>
2022-09-15 12:57:31 +0800

    GitHubShare

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/Qubit-Fernand/nextjs-notion-starter-kit'
+      href='https://github.com/Qubit-Fernand'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
