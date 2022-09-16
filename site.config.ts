cb917db7211edb2041ae28f819f08153185bc602
AntiEntropy-bit <AntiEntropy@pku.edu.cn>
2022-09-16 19:50:57 +0800

    Ver 2.0

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: 'e57fa69576c34239932f087196971d29',
 
   // basic site info (required)
   name: 'Introduction to Quantum Computing',
