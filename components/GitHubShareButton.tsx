2b75d5e39f4af1a6dcde4e3e31166c1003de5a73
AntiEntropy-bit <AntiEntropy@pku.edu.cn>
2023-07-26 11:12:08 +0800

    Google Scholar

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/Qubit-Fernand'
+      href='https://github.com/Qubit-Fernand/Quantum-Computing'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
