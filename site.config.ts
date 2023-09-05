0ff46c664d2473d735b938e4874b2e64566d8bb3
AntiEntropy-bit <AntiEntropy@pku.edu.cn>
2023-09-05 21:26:50 +0800

    Initialize

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'bcd9bf4227214133b3481d30ef4ceaba',
+  rootNotionPageId: 'e3bf1aee47f04677a3b005d7e97165bd',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
